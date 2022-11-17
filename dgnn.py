72701a1a140a6b09985d5653b9bdfa70cb58c80d
Ondra <ondra.baudisch@centrum.cz>
2022-11-17 20:45:20 +0100

    a lot of changes

--- dgnn.py
+++ dgnn.py
@@ -1,6 +1,11 @@
+import os
+
+os.environ["NUMBA_ENABLE_CUDASIM"] = "0"
+
 import numpy as np
 from activation_fn import Relu, Identity
 from numba import cuda
+import math
 
 class DGNN:
     def __init__(self, n_inputs: int, n_outputs: int, hiden_activation, last_activation):
@@ -23,6 +28,7 @@ class DGNN:
         self._inicialize()
     
     def _inicialize(self):
+        self.memory = np.zeros([1, 2, self.n_inputs + self.n_outputs], dtype=np.float64)
         self.weights = np.full([self.n_outputs, self.n_outputs + self.n_inputs], fill_value=None, dtype=np.float64)
         #fill weights randomly
         for i in range(self.n_inputs):
@@ -35,20 +41,6 @@ class DGNN:
         self.neuron_info[0].T[1] = np.arange(0, self.n_outputs, 1, dtype=np.int64)
 
         self.max_layer_len = self.n_outputs
-
-    def Prepare_memory(self, n_sequences: int=1):
-        """
-        Prepare the memory for training or calculation.\n
-
-        Parameters
-        ----------
-        n_sequences:
-            Number of sequences\n
-            Is intended for beckprop, it determines how many sequences beckprop can process at once.\n
-            If you only want to perform the calculation, leave the value at 1.
-        """
-
-        self.memory = np.zeros([n_sequences, 2, self.n_inputs + self.n_outputs], dtype=np.float64)
 
     def Calculate_CPU(self, inputs: np.ndarray, clear_mem: bool=False) -> np.ndarray:
         if clear_mem:
@@ -78,9 +70,9 @@ class DGNN:
         Creates a kernel from computing beckprop and calculation, and moves network parameters to gpu
         """
         # mov all parameters to gpu
-        self.cuda_weights = cuda.to_device(np.zeros_like(self.weights))
+        self.cuda_weights = cuda.to_device(self.weights)
         self.cuda_neuron_info = cuda.to_device(self.neuron_info)
-        self.cuda_memory = cuda.to_device(self.memory)
+        self.cuda_memory = cuda.to_device(np.zeros_like(self.memory))
 
         # compile all functions to gpu
         hiden_fn = cuda.jit(self.hiden_activation.cuda_fn(), device=True)
@@ -89,11 +81,33 @@ class DGNN:
         last_derivative = cuda.jit(self.last_activation.cuda_derivative(), device=True)
 
         @cuda.jit
-        def kernel_calculate_one(weights_io, neuron_info_io, memory_io, inputs_io):
-            for inp in inputs_io:
-                if inp.shape[0] > cuda.threadIdx.x:
-                    memory_io[0, 0, cuda.threadIdx.x] = inp[cuda.threadIdx.x]
+        def kernel_calculate_one(weights_io, neuron_info_io, memory_io, inputs_io, outputs_io):
+            for inpidx, inp in enumerate(inputs_io):
+                for idx in range(math.ceil(inp.shape[0] / cuda.blockDim.x)):
+                    idx_mem_mov = cuda.threadIdx.x + cuda.blockDim.x * idx
+                    if inp.shape[0] > idx_mem_mov:
+                        memory_io[0, 0, idx_mem_mov] = inp[idx_mem_mov]
                 cuda.syncthreads()
+
+                for layer_info in neuron_info_io:
+                    if math.isnan(layer_info[cuda.threadIdx.x][2]): break
+
+                    result = 0
+                    for weight, mem in zip(weights_io[layer_info[cuda.threadIdx.x, 1]], memory_io[0, 0]):
+                        if math.isnan(weight): weight = 0
+
+                        result += weight * mem
+                    result += layer_info[cuda.threadIdx.x, 0]
+                    if layer_info[cuda.threadIdx.x, 3] == 1:
+                        result = hiden_fn(result)
+                    else:
+                        result = last_fn(result)
+                        outputs_io[inpidx, cuda.threadIdx.x] = result
+                    
+
+                    cuda.syncthreads()
+                    memory_io[0, 0, layer_info[cuda.threadIdx.x, 2]] = result
+                    cuda.syncthreads()
 
         def kernel_calculate_full():
             raise NotImplementedError
@@ -111,12 +125,18 @@ class DGNN:
         cuda_inputs = cuda.to_device(inputs)
         cuda_outputs = cuda.to_device(np.zeros([inputs.shape[0], self.n_outputs]))
 
-        self._kernel_calculate_one[1, self.max_layer_len](self.cuda_weights, self.cuda_neuron_info, self.cuda_memory, cuda_inputs)
-        return self.cuda_memory.copy_to_host()
+        self._kernel_calculate_one[1, self.max_layer_len](self.cuda_weights, self.cuda_neuron_info, self.cuda_memory, cuda_inputs, cuda_outputs)
+        
+        return cuda_outputs.copy_to_host()
+
+    def Beckprop(self, training_data: list, clear_mem: bool=False) -> int:
+        raise NotImplementedError
 
         
 
 if __name__ == '__main__':
+    from pprint import pprint
+
     np.random.seed(8)
     dgnn = DGNN(2, 4, Relu(), Identity())
     dgnn.Prepare_memory()
@@ -125,9 +145,10 @@ if __name__ == '__main__':
         [1, 2],
         [3, 4],
         [0, -1],
-        [1, 1]
+        [1, -2]
     ]))
-    print(out)
+    pprint(out)
+
     # dgnn.Claculate_slow(np.array([
     #     [[1, 2], [3, 4]],
     #     [[2, 1], [6, 5]],
