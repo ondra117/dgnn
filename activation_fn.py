da421729f69f552520b13b026b580417bb5d661c
Ondra <ondra.baudisch@centrum.cz>
2022-11-17 12:17:03 +0100

    a lot of changes

---
+++ activation_fn.py
@@ -0,0 +1,71 @@
+from abc import ABC, abstractmethod
+
+class Activation_fn(ABC):
+    def __init__(self, **kwargs):
+        super().__init__()
+
+    def __call__(self, z):
+        return self.fn(z)
+
+    @abstractmethod
+    def fn(self, z):
+        raise NotImplementedError
+
+    @abstractmethod
+    def derivative(self, z):
+        raise NotImplementedError
+
+    @abstractmethod
+    def cuda_fn(self, z):
+        raise NotImplementedError
+
+    @abstractmethod
+    def cuda_derivative(self, z):
+        raise NotImplementedError
+
+class Relu(Activation_fn):
+    def __init__(self):
+        """A rectified linear ctivation function."""
+        super().__init__()
+
+    def __str__(self):
+        return "ReLU"
+
+    def fn(self, z):
+        return max(0, z)
+
+    def derivative(self, z):
+        return 0 if z < 0 else 1
+
+    def cuda_fn(self):
+        def fn(z):
+            return max(0, z)
+        return fn
+
+    def cuda_derivative(self):
+        def fn(z):
+            return 0 if z < 0 else 1
+        return fn
+
+class Identity(Activation_fn):
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return "Identity"
+
+    def fn(self, z):
+        return z
+
+    def derivative(self, z):
+        return 1
+
+    def cuda_fn(z):
+        def fn(z):
+            return z
+        return fn
+    
+    def cuda_derivative(z):
+        def fn(z):
+            return 1
+        return fn
--- dgnn.py
+++ dgnn.py
@@ -1,7 +1,95 @@
 import numpy as np
+from activation_fn import Relu, Identity
 
 class DGNN:
-    pass
+    def __init__(self, n_inputs: int, n_outputs: int, hiden_activation, last_activation):
+        """
+        Directed Graph Neural Network
+
+        Parameters
+        ----------
+        n_inputs:
+            number of inputs
+        n_outputs:
+            number of outputs
+        """
+        #save all parameters
+        self.n_inputs = n_inputs
+        self.n_outputs = n_outputs
+        self.hiden_activation = hiden_activation
+        self.last_activation = last_activation
+
+        self._inicialize()
+    
+    def _inicialize(self):
+        self.weights = np.full([self.n_outputs, self.n_outputs + self.n_inputs], fill_value=None, dtype=np.float64)
+        #fill weights randomly
+        for i in range(self.n_inputs):
+            self.weights.T[i] = np.random.normal(size=[self.weights.shape[0]])
+        #there is saved bias, index of weights, index of memory, activation
+        self.neuron_info = np.zeros([1, self.n_outputs, 4], dtype=np.int64)
+        #fill index of memory
+        self.neuron_info[0].T[2] = np.arange(self.n_inputs, self.n_inputs + self.n_outputs, 1, dtype=np.int64)
+        #fill index of weights
+        self.neuron_info[0].T[1] = np.arange(0, self.n_outputs, 1, dtype=np.int64)
+
+        self.max_layer_len = self.n_outputs
+
+    def Prepare_memory(self, n_sequences: int=1):
+        """
+        Prepare the memory for training or calculation.\n
+
+        Parameters
+        ----------
+        n_sequences:
+            Number of sequences\n
+            Is intended for beckprop, it determines how many sequences beckprop can process at once.\n
+            If you only want to perform the calculation, leave the value at 1.
+        """
+
+        self.memory = np.zeros([n_sequences, 2, self.n_inputs + self.n_outputs], dtype=np.float64)
+
+    def Calculate_CPU(self, inputs: np.ndarray, clear_mem: bool=True) -> np.ndarray:
+        if clear_mem:
+            self.memory.fill(0)
+        temporary_memory = np.copy(self.memory)
+        output = np.zeros([inputs.shape[0], self.n_outputs])
+
+        for idx, inp in enumerate(inputs):
+            self.memory[0][0][:self.n_inputs] = inp
+            
+            for layer_info in self.neuron_info:
+                for neuron_info in layer_info:
+                    result = np.sum(np.nan_to_num(self.weights[neuron_info[1]]) * self.memory[0][0]) + neuron_info[0]
+                    if neuron_info[3]:
+                        result = self.hiden_activation(result)
+                    else:
+                        result = self.last_activation(result)
+                    temporary_memory[0][0][neuron_info[2]] = result
+                self.memory[0][0] = temporary_memory[0][0]
+            
+            output[idx] = self.memory[0][0][-self.n_outputs:]
+        
+        return output
+
+    
+        
+
+        
 
 if __name__ == '__main__':
-    pass
+    np.random.seed(8)
+    dgnn = DGNN(2, 4, Relu(), Identity())
+    dgnn.Prepare_memory()
+    out = dgnn.Calculate_CPU(np.array([
+        [1, 2],
+        [3, 4],
+        [0, -1],
+        [1, 1]
+    ]))
+    print(out)
+    # dgnn.Claculate_slow(np.array([
+    #     [[1, 2], [3, 4]],
+    #     [[2, 1], [6, 5]],
+    #     [[6, 6], [1, 1]]
+    # ]))
